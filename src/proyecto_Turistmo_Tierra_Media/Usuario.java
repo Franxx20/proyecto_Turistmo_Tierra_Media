a9d104da0aec1cfd4c8d37f8625d6b69f3398009
Franxx20 <francojg2013.Fg@gmail.com>
2023-05-26 21:13:43 -0300

    me olvide de esto

--- src/proyecto_Turistmo_Tierra_Media/Usuario.java
+++ src/proyecto_Turistmo_Tierra_Media/Usuario.java
@@ -3,50 +3,48 @@ package proyecto_Turistmo_Tierra_Media;
 import java.util.ArrayList;
 
 public class Usuario {
-	private final String nombre;
-	private final TipoDePaquete preferencia;
-	private final int presupuesto;
-	private final double tiempoDisponible;
-	private final ArrayList<Atraccion> itinerario;
+  private final String nombre;
+  private final TipoDePaquete preferencia;
+  private final int presupuesto;
+  private final double tiempoDisponible;
+  private ArrayList<Producto> itinerario;
 
+  public Usuario(String nombre, TipoDePaquete preferencia, int presupuesto, double tiempoDisponible,
+      ArrayList<Producto> itinerario) {
+    this.nombre = nombre;
+    this.preferencia = preferencia;
+    this.presupuesto = presupuesto;
+    this.tiempoDisponible = tiempoDisponible;
+    this.itinerario = itinerario;
+  }
 
-	public Usuario(String nombre, TipoDePaquete preferencia, int presupuesto, double tiempoDisponible,
-			ArrayList<Atraccion> itinerario) {
-		super();
-		this.nombre = nombre;
-		this.preferencia = preferencia;
-		this.presupuesto = presupuesto;
-		this.tiempoDisponible = tiempoDisponible;
-		this.itinerario = itinerario;
-	}
-	
-	@Override
-	public String toString() {
-		return "Usuario [nombre=" + nombre + ", preferencia=" + preferencia + ", presupuesto=" + presupuesto
-				+ ", tiempoDisponible=" + tiempoDisponible + ", itinerario=" + itinerario + "]"+ "\n";
-	}
+  @Override
+  public String toString() {
+    return "Usuario [nombre=" + nombre + ", preferencia=" + preferencia + ", presupuesto=" + presupuesto
+        + ", tiempoDisponible=" + tiempoDisponible + ", itinerario=" + itinerario + "]" + "\n";
+  }
 
-	public String getNombre() {
-		return nombre;
-	}
+  public String getNombre() {
+    return nombre;
+  }
 
-	public TipoDePaquete getPreferencia() {
-		return preferencia;
-	}
+  public TipoDePaquete getPreferencia() {
+    return preferencia;
+  }
 
-	public int getPresupuesto() {
-		return presupuesto;
-	}
+  public int getPresupuesto() {
+    return presupuesto;
+  }
 
-	public double getTiempoDisponible() {
-		return tiempoDisponible;
-	}
+  public double getTiempoDisponible() {
+    return tiempoDisponible;
+  }
 
-	public ArrayList<Atraccion> getItinerario() {
-		return itinerario;
-	}
+  public ArrayList<Atraccion> getItinerario() {
+    return itinerario;
+  }
 
-	public void agregarAItinerario(Atraccion atraccion){
-		this.itinerario.add(atraccion);
-	}
+  public void agregarAItinerario(Atraccion atraccion) {
+    this.itinerario.add(atraccion);
+  }
 }
